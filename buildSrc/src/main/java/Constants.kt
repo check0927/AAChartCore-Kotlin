6bb0c48c21669a1f226428ceadb22c8acfb6cd93
YeQiu <527131858@qq.com>
2022-09-07 17:54:15 +0800

    Update Constants.kt

--- buildSrc/src/main/java/Constants.kt
+++ buildSrc/src/main/java/Constants.kt
@@ -3,9 +3,9 @@ import org.gradle.plugin.use.PluginDependencySpec
 
 object AndroidConstants {
     const val minSdkVersion = 19
-    const val targetSdkVersion = 30
-    const val compileSdkVersions = 30
-    const val buildToolsVersion = "30.0.2"
+    const val targetSdkVersion = 29
+    const val compileSdkVersions = 31
+    const val buildToolsVersion = "29.0.3"
 }
 
 object Versions {
